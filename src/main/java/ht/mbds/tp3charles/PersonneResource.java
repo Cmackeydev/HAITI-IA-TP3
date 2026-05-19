23000b2e52c12c36ee653be047d85d1461af5b5a
Mackey CHARLES <cmackey016@yahoo.fr>
2026-05-18 21:22:48 -0400

    Changement du type de la reponse

--- src/main/java/ht/mbds/tp3charles/PersonneResource.java
+++ src/main/java/ht/mbds/tp3charles/PersonneResource.java
@@ -4,10 +4,11 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.Response;
 
 @Path("/Personnes/{nom}")
 public class PersonneResource {
     @GET
     @Produces("text/plain")
-    public String hello(@PathParam("nom") String nom) {return "Hello, " + nom;}
+    public Response hello(@PathParam("nom") String nom) {return Response.ok("Hello, " + nom).build();}
 }
